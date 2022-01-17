f0839dfc638b758fa569bb34e1cc41c50a0e5466
Aleksey <zawertuy@gmail.com>
2022-01-17 19:46:09 +1200

    Lesson-2

--- Lesson-2/routes/web.php
+++ Lesson-2/routes/web.php
@@ -21,6 +21,13 @@ Route::get('/greeting', function () {
     return 'Nice to see you!';
 });
 
-Route::get('/anotherView', function () {
-    return view('anotherView');
-});
+// Route::get('/anotherView', function () {
+//     return view('anotherView');
+// });
+
+Route::get('/category_news', [App\Http\Controllers\MainController::class, 'index']);
+
+Route::get('/category_news/{id}', [App\Http\Controllers\MainController::class, 'showCategory']);
+
+Route::get('/authorization', [App\Http\Controllers\AuthController::class, 'index']);
+
